08ad73a89feec62ee5c860194f81c09dc43a676b
Michael <mivanovic800@gmail.com>
2026-06-21 23:17:34 +0300

    Micro micro fix
    I need to st0p update this every second

--- Dowload Youtube Music Tracks from file.py
+++ Dowload Youtube Music Tracks from file.py
@@ -1,8 +1,8 @@
 import project_module
 music_dir = ""
 def main():
-    print(f"Получение ссылок из файла - {project_module.files_paths.Youtube_Music_Playlists_links}")
-    urls = project_module.read_links_from_file(project_module.files_paths.Youtube_Music_Playlists_links)
+    print(f"Получение ссылок из файла - {project_module.files_paths.Youtube_Music_links}")
+    urls = project_module.read_links_from_file(project_module.files_paths.Youtube_Music_links)
     for url in urls:
         print(url)
         try:
