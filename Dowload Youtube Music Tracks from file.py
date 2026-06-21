8df68f2897e01ed36a1afed209b094b0ac3d85e8
Michael <mivanovic800@gmail.com>
2026-06-21 23:14:03 +0300

    one small fix

--- Dowload Youtube Music Tracks from file.py
+++ Dowload Youtube Music Tracks from file.py
@@ -1,2 +1,12 @@
 import project_module
-project_module.download_youtube_track_with_info(input("Введи ссылку на ютуб мьюзик трек\n>"))
+music_dir = ""
+def main():
+    print(f"Получение ссылок из файла - {project_module.files_paths.Youtube_Music_Playlists_links}")
+    urls = project_module.read_links_from_file(project_module.files_paths.Youtube_Music_Playlists_links)
+    for url in urls:
+        print(url)
+        try:
+            project_module.download_youtube_track_with_info(url)
+        except:
+            print(f"Что то пошло не так с {url}")
+main()
