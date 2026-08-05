161bd4a1569765c5adf2dda91c2543ccebb0c8c1
Michael <mivanovic800@gmail.com>
2026-08-05 13:19:54 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,4 @@
+import project_module
+params = project_module.input_album_parametrs()
+project_module.download_soundcloud_playlist("https://soundcloud.com/after_f/sets/test",params)
+project_module.download_youtube_music_playlist("http://youtube.com/watch?v=VDEZ61vP7S0&list=PLNB9-hdaPTm0&index=1",params)
